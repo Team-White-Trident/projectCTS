b7faec39394f0f5ee812578626a31bdc87ff10cd
Teamwhitetrident <Teamwhitetrident@gmail.com>
2017-04-11 12:27:27 +0530

    basicModifications

--- public/js/profilePage.js
+++ public/js/profilePage.js
@@ -2,12 +2,12 @@
 $(document).on("click", ".open-profileDialog", function () {
      var contentId = $(this).data('id');
      var contentWhatever = $(this).data('whatever');
-     var contentLang = $(this).data('lang').trim();
+    // var contentLang = $(this).data('lang').trim();
 
-    $(".modal-header").html(contentId);
-    $("#codearea").attr("data-language",contentLang);
+    $(".modal-header").html(contentId.bold());
+  //  $("#codearea").attr("data-language",contentLang);
 
-    //  $(".modal-b #modalbody").html( contentWhatever );
+    //$(".modal-body #modalbody").html( contentWhatever );
 
      $("#codearea").html( contentWhatever );
      //$( "#test" ).load( "../../ #test" );
